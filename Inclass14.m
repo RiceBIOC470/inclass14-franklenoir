47390511439d77744352170c5be46a971c61e61b
franklenoir <franklenoir@users.noreply.github.com>
2017-10-17 15:47:52 -0500

    Inclass 14
    Submitting Inclass 14
    Walter Frank Lenoir

--- Inclass14.m
+++ Inclass14.m
@@ -1,8 +1,39 @@
 %Inclass 14
-
+%Walter Frank Lenoir
 %Work with the image stemcells_dapi.tif in this folder
-
+reader1 = bfGetReader('stemcells_dapi.tif');
+iplane = reader1.getIndex(1-1,1-1,1-1)+1;
+img = bfGetPlane(reader1,iplane);
 % (1) Make a binary mask by thresholding as best you can
+imshow(img,[])
+mask = img > 350;
+imshow(mask,[])
+
 % (2) Try to separate touching objects using watershed. Use two different
 % ways to define the basins. (A) With erosion of the mask (B) with a
 % distance transform. Which works better in this case?
+%a) 
+CC = bwconncomp(mask);
+stats = regionprops(CC,'Area');
+area = [stats.Area];
+
+s = round(1.2*sqrt(mean(area))/pi);
+erodemask = imerode(mask,strel('disk',s));
+outside = ~imdilate(mask,strel('disk',1));
+basin = imcomplement(bwdist(outside));
+basin = imimposemin(basin,erodemask|outside);
+L = watershed(basin);
+imshow(L, [])
+
+%b) 
+D = bwdist(~mask);
+imshow(D,[])
+D = -D;
+D(~mask) = -Inf;
+L2 = watershed(D);
+imshow(L2,[])
+
+%The erode method got rid of a few cells, however its the better method for
+%seperating objects with this image. The distance transformation worked
+%well, however caused too many breaks in the final product (some cells were
+%unnecessarily divided).
